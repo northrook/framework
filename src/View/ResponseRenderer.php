10a3e8543165d2242f50b1cd1eda62c6a55a6062
Martin <hello@northrook.com>
2024-11-12 17:57:50 +0000

    mend

--- src/View/ResponseRenderer.php
+++ src/View/ResponseRenderer.php
@@ -10,6 +10,34 @@ final class ResponseRenderer
 
     public function __invoke( ResponseEvent $event ) : void
     {
-        dump( $event, $this );
+        if ( ! $event->getRequest()->attributes->get( '_request_type' ) ) {
+            return;
+        }
+
+        [$htmx_request, $request_type, $content_template, $document_template] = $this->getAttributes( $event );
+
+        dump( $this, $htmx_request, $request_type, $content_template, $document_template );
+
+        if ( $content_template ) {
+            dump(
+                $this->templateEngine->render( $content_template, [] ),
+            );
+        }
+    }
+
+    private function getAttributes( ResponseEvent $event ) : array
+    {
+        $attributes = \array_intersect_key(
+            [
+                '_htmx_request'      => null,
+                '_request_type'      => null,
+                '_content_template'  => null,
+                '_document_template' => null,
+            ],
+            $event->getRequest()->attributes->all(),
+        );
+
+        dump( $attributes );
+        return $attributes;
     }
 }
