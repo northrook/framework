18a83e44c0dfe261f9374fe09bfcfe3c39008db7
Martin <hello@northrook.com>
2024-11-15 13:32:07 +0000

    mend

--- src/UI/Component/Heading.php
+++ src/UI/Component/Heading.php
@@ -2,4 +2,45 @@
 
 namespace Core\UI\Component;
 
-class Heading {}
+use Core\UI\Component;
+use Core\View\Render\ComponentInterface;
+use Core\View\Template\Compiler\NodeCompiler;
+use Core\View\Template\Render;
+use Latte\Compiler\Nodes\AuxiliaryNode;
+use Psr\Log\LoggerInterface;
+
+final class Heading extends Component
+{
+    protected function build() : string
+    {
+        return (string) $this->element;
+    }
+
+    public static function create(
+        array            $arguments,
+        array            $autowire = [],
+        ?string          $uniqueId = null,
+        ?LoggerInterface $logger = null,
+    ) : ComponentInterface {
+        return new self( 'h1' );
+    }
+
+    public static function templateNode( NodeCompiler $node ) : AuxiliaryNode
+    {
+        $attributes = $node->attributes();
+
+        $href = $node->arguments()['href'] ?? $attributes['href'] ?? null;
+
+        unset( $attributes['href'] );
+
+        return Render::auxiliaryNode(
+            self::componentName(),
+            [
+                'href'       => $href,
+                'attributes' => $node->attributes(),
+                'content'    => $node->parseContent(),
+                'tag'        => $node->tag,
+            ],
+        );
+    }
+}
--- src/UI/Component/Icon.php
+++ src/UI/Component/Icon.php
@@ -2,4 +2,61 @@
 
 namespace Core\UI\Component;
 
-class Icon {}
+use Core\UI\Component;
+use Core\View\Render\ComponentInterface;
+use Core\View\Template\Compiler\NodeCompiler;
+use Core\View\Template\Render;
+use Latte\Compiler\Nodes\AuxiliaryNode;
+use Psr\Log\LoggerInterface;
+use ValueError;
+
+final class Icon extends Component
+{
+    public function __construct(
+        string           $get,
+        array            $attributes = [],
+        ?string          $tooltip = null,
+        string           $tag = 'i',
+        ?string          $uniqueId = null,
+        ?LoggerInterface $logger = null,
+    ) {
+        parent::__construct( $tag, $attributes, [], $uniqueId, $logger );
+    }
+
+    protected function build() : string
+    {
+        return (string) $this->element;
+    }
+
+    public static function create(
+        array            $arguments,
+        array            $autowire = [],
+        ?string          $uniqueId = null,
+        ?LoggerInterface $logger = null,
+    ) : ComponentInterface {
+        $get        = $arguments['get']        ?? throw new ValueError( 'No [icon get] value is provided.' );
+        $attributes = $arguments['attributes'] ?? [];
+        $content    = $arguments['content']    ?? '';
+        $tag        = $arguments['tag']        ?? 'a';
+        return new self( 'i' );
+    }
+
+    public static function templateNode( NodeCompiler $node ) : AuxiliaryNode
+    {
+        $attributes = $node->attributes();
+
+        $href = $node->arguments()['href'] ?? $attributes['href'] ?? null;
+
+        unset( $attributes['href'] );
+
+        return Render::auxiliaryNode(
+            self::componentName(),
+            [
+                'href'       => $href,
+                'attributes' => $node->attributes(),
+                'content'    => $node->parseContent(),
+                'tag'        => $node->tag,
+            ],
+        );
+    }
+}
