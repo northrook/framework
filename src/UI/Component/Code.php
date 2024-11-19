bad5e7470da739a9ecbe7f7c421cc963f2782c56
Martin <hello@northrook.com>
2024-11-19 12:50:36 +0000

    mend

---
+++ src/UI/Component/Code.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Core\UI\Component;
+
+use Core\View\Attribute\ComponentNode;
+use Core\View\Component\ComponentBuilder;
+use Core\View\Template\Compiler\NodeCompiler;
+use Core\View\Template\Render;
+use Latte\Compiler\Nodes\AuxiliaryNode;
+
+#[ComponentNode( ['pre', 'code:{lang}:block'], 'static' )]
+final class Code extends ComponentBuilder
+{
+    protected const ?string TAG = 'code';
+
+    protected bool $tidy = false;
+
+    protected ?string $lang = null;
+
+    protected ?string $block = null;
+
+    private string $code;
+
+    protected function parseArguments( array &$arguments ) : void
+    {
+        $this->code = $arguments['content'] ?? null;
+
+        unset( $arguments['content'] );
+    }
+
+    protected function compile() : string
+    {
+        dump( $this );
+        return (string) $this->component;
+    }
+
+    public function templateNode( NodeCompiler $node ) : AuxiliaryNode
+    {
+        return Render::templateNode(
+            self::componentName(),
+            $this::nodeArguments( $node ),
+        );
+    }
+
+    public static function nodeArguments( NodeCompiler $node ) : array
+    {
+        return [
+            'tag'        => $node->tag,
+            'attributes' => $node->attributes(),
+            'content'    => $node->parseContent(),
+        ];
+    }
+}
