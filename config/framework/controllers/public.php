6ce46b95fb2c42e5cc1535878123a67d9fe66c1e
Martin <hello@northrook.com>
2024-11-30 14:12:46 +0000

    mend

--- config/framework/controllers/public.php
+++ config/framework/controllers/public.php
@@ -8,9 +8,13 @@ declare(strict_types=1);
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
-use Core\Controller\PublicController;
-
+use Core\Controller\{FaviconController, PublicController};
 return static function( ContainerConfigurator $controller ) : void {
+    $controller->services()
+        ->set( FaviconController::class )
+        ->tag( 'controller.service_arguments' )
+        ->tag( 'monolog.logger', ['channel' => 'request'] );
+
     $controller->services()
         ->set( PublicController::class )
         ->tag( 'controller.service_arguments' )
