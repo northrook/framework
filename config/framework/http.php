40c332dc28a8ffbc38a3815c801025c2e77ba0c1
Martin <hello@northrook.com>
2024-12-04 10:04:27 +0000

    mend

--- config/framework/http.php
+++ config/framework/http.php
@@ -8,15 +8,19 @@ declare(strict_types=1);
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
+use Northrook\Clerk;
+
 return static function( ContainerConfigurator $container ) : void {
     $container->services()
             //
         ->set( \Core\HTTP\RequestListener::class )
+        ->args( [service( Clerk::class )] )
         ->tag( 'kernel.event_subscriber' )
         ->tag( 'monolog.logger', ['channel' => 'http'] )
 
             //
         ->set( \Core\HTTP\ResponseListener::class )
+        ->args( [service( Clerk::class )] )
         ->tag( 'kernel.event_subscriber' )
         ->tag( 'monolog.logger', ['channel' => 'http'] );
 };
