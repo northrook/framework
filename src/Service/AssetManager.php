4ce0b9d294b98e02a3380a549bfe49a0a5b6e353
Martin <hello@northrook.com>
2024-12-20 13:09:58 +0000

    mend

--- src/Service/AssetManager.php
+++ src/Service/AssetManager.php
@@ -11,25 +11,30 @@ use Symfony\Contracts\Cache\CacheInterface;
 class AssetManager extends \Core\Assets\AssetManager
 {
     final public function __construct(
-        AssetFactory     $factory,
-        ?CacheInterface  $cache = null,
-        ?LoggerInterface $logger = null,
-    ) {
+            AssetFactory     $factory,
+            ?CacheInterface  $cache = null,
+            ?LoggerInterface $logger = null,
+    )
+    {
         parent::__construct( $factory, $cache, $logger );
 
         $this->factory->addAssetModelCallback(
-            'style.core',
-            function( StyleAsset $asset ) : StyleAsset {
-                $style = new StyleFramework();
+                'style.core',
+                function( StyleAsset $asset ) : StyleAsset
+                {
+                    $style = new StyleFramework();
 
-                $asset->addSource( $style->style() );
+                    $asset->addSource( $style->style() );
 
-                $localAssets = $asset->pathfinder->get( 'dir.core.assets/styles/core' );
+                    $localAssets = $asset->pathfinder->getFileInfo( 'dir.core.assets/styles/core' );
 
-                dump( $localAssets );
+                    foreach ( $localAssets->glob( '/*.css' ) as $localAsset ) {
+                        dump( $localAsset );
+                        $asset->addSource( $localAsset );
+                    }
 
-                return $asset;
-            },
+                    return $asset;
+                },
         );
     }
 }
