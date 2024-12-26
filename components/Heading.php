ed157720e86ed4ae8918c6a7a72d7dea2dd90785
Martin <hello@northrook.com>
2024-12-26 14:09:15 +0000

    mend

--- components/Heading.php
+++ components/Heading.php
@@ -6,23 +6,23 @@ namespace Core\View\Component;
 
 use Core\View\Attribute\ViewComponent;
 use Core\View\{Component};
-use Core\View\Template\TemplateCompiler;
+// use Core\View\Template\TemplateCompiler;
 use Northrook\HTML\Element\Tag;
 
 #[ViewComponent( Tag::HEADING, true, 128 )]
-final class Heading extends Component
+final class Heading
 {
     public string $get;
 
     public string $tag = 'h1';
-
-    protected function parseArguments( array &$arguments ) : void
-    {
-        dump( $arguments );
-    }
-
-    protected function compile( TemplateCompiler $compiler ) : string
-    {
-        return $compiler->render( __DIR__.'/heading.latte', $this, cache : false );
-    }
+    //
+    // protected function parseArguments( array &$arguments ) : void
+    // {
+    //     dump( $arguments );
+    // }
+    //
+    // protected function compile( TemplateCompiler $compiler ) : string
+    // {
+    //     return $compiler->render( __DIR__.'/heading.latte', $this, cache : false );
+    // }
 }
