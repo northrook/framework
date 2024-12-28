d64e64d6ba29e290318bb28ea9c1148cc52609cc
Martin <hello@northrook.com>
2024-12-28 08:36:03 +0000

    mend

--- src/View/Component/Icon.php
+++ src/View/Component/Icon.php
@@ -6,14 +6,37 @@ namespace Core\View\Component;
 
 use Core\Service\IconService;
 use Core\View\Attribute\ViewComponent;
+use Core\View\Html\{Attributes, Tag};
 
 #[ViewComponent( 'icon:{get}', true, 128 )]
 final class Icon extends AbstractComponent
 {
+    protected string $get;
+
+    protected readonly Tag $tag;
+
+    protected readonly Attributes $attributes;
+
     public function __construct(
         private readonly IconService $iconService,
     ) {
-        dump( $this );
+        $this->tag        = Tag::from( 'i' );
+        $this->attributes = new Attributes();
+    }
+
+    protected function render() : string
+    {
+        $iconHtml = $this->iconService->getIcon( $this->get );
+
+        if ( ! $iconHtml ) {
+            return '';
+        }
+
+        return <<<HTML
+            <i{$this->attributes}>
+            {$iconHtml}
+            </i>
+            HTML;
     }
 }
 
